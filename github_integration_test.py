cda3ba5994f4196f28f9625892f3ce367e855fc1
tomprogramuje <tom.severa@outlook.cz>
2024-04-16 13:26:42 +0200

    [RED] added 'test_fetch_github_events_failure'

--- github_integration_test.py
+++ github_integration_test.py
@@ -23,8 +23,24 @@ def mock_requests_get_success(monkeypatch):
 
     monkeypatch.setattr("requests.get", mock_get)
 
+
+@pytest.fixture
+def mock_requests_get_failure(monkeypatch):
+    def mock_get(*args, **kwargs):
+        mock_response = MockResponse(None, 404, "Not Found")
+        return mock_response
+
+    monkeypatch.setattr("requests.get", mock_get)
+
+
 def test_fetch_github_events_success(mock_requests_get_success):
     events = fetch_github_events("owner/repo1", "your_access_token")
 
     assert len(events) == 3
     assert events["type"] == "PullRequestEvent"
+
+
+def test_fetch_github_events_failure(mock_requests_get_failure):
+    events = fetch_github_events("owner/repo1", "your_access_token")
+
+    assert events is None
