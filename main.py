137391eba8f48ef89c675c106319ec17df3161f5
tomprogramuje <tom.severa@outlook.cz>
2024-04-16 21:20:48 +0200

    [REFACTOR] 'home' function changed to call api.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
+import api
+
 app = FastAPI()
 
 
 @app.get("/")
 async def home():
-    data = {"message": "Hello World!"}
-    return JSONResponse(content=data)
+    data = api.handle_home()
+    response = {"message": data}
+    return JSONResponse(content=response)
