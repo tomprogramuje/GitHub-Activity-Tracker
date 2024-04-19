06f48b09ddca96b3392caeba7173ce6a5c3ddc49
tomprogramuje <tom.severa@outlook.cz>
2024-04-19 09:27:17 +0200

    [GREEN] 'test_handle_events_success' passes

--- api.py
+++ api.py
@@ -1,2 +1,10 @@
+import github_integration
+
+
 def handle_home() -> str:
     return "Hello World!"
+
+
+def handle_events(owner_repo, token):
+    events = github_integration.fetch_github_events(owner_repo, token)
+    return events
